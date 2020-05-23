8a358a447bbccce3d24af98c69605da5bc46dd16
Juhana Jauhiainen <juhana.jauhiainen@gmail.com>
2020-05-23 23:43:25 +0300

    Initial commit

---
+++ components/PokeCard.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from "react";
+import useSWR from "swr";
+
+import { find, map } from "lodash";
+
+async function getPokemon(key) {
+  const response = await fetch(`https://pokeapi.co/${key}`);
+  return await response.json();
+}
+
+function capitalize(input) {
+  return input[0].toUpperCase() + input.slice(1);
+}
+
+const colors = {
+  fire: "#FDDFDF",
+  grass: "#DEFDE0",
+  electric: "#FCF7DE",
+  water: "#DEF3FD",
+  ground: "#f4e7da",
+  rock: "#d5d5d4",
+  fairy: "#fceaff",
+  poison: "#98d7a5",
+  bug: "#f8d5a3",
+  dragon: "#97b3e6",
+  psychic: "#eaeda1",
+  flying: "#F5F5F5",
+  fighting: "#E6E0D4",
+  normal: "#F5F5F5",
+};
+
+export default function PokeCard({ id }) {
+  const { data, error } = useSWR(`api/v2/pokemon/${id}`, getPokemon);
+
+  if (!data) {
+    return (
+      <div className="flex flex-col justify-center w-40 h-64 text-center align-middle bg-white shadow-lg rounded-large">
+        Loading
+      </div>
+    );
+  }
+
+  const imageUrl = `https://pokeres.bastionbot.org/images/pokemon/${data.id}.png`;
+  const name = capitalize(data.name);
+
+  const mainTypes = Object.keys(colors);
+  const types = map(data.types, (t) => t.type.name);
+  const pokemonType = find(mainTypes, (t) => types.indexOf(t) > -1);
+  const color = colors[pokemonType];
+  console.log(color);
+
+  return (
+    <div
+      style={{ backgroundColor: color }}
+      className="flex flex-wrap justify-center w-48 p-5 shadow-lg h-84 rounded-xl"
+    >
+      <div className="flex bg-white bg-opacity-50 rounded-full">
+        <img className="w-full" src={imageUrl} alt={data.name} />
+      </div>
+      <div>
+        <div className="p-1 pl-2 pr-2 mt-4 ml-8 mr-8 text-sm text-center bg-gray-800 bg-opacity-25 rounded-xl">
+          #{id.toString().padStart(3, "0")}
+        </div>
+        <div className="mt-1 text-center">{name}</div>
+        <div className="mt-1 text-xs text-center">Type: {pokemonType}</div>
+      </div>
+    </div>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      spacing: {
+        "1/2": "4px",
+        "72": "18rem",
+        "84": "21rem",
+        "96": "24rem",
+      },
+      borderRadius: {
+        xl: "0.75rem",
+        xl2: "1.25rem",
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
